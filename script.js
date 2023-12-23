eb4dd226959d1663c02db74329e75b807195692b
AnkurGarg07git config --global user.email Ankurgarg89p@gmail.comgit config --listgit config --global user.name AnkurGarg07 <Ankurgarg89p@gmail.com>
2023-12-23 13:47:41 +0530

    bug fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".addIcon");
 let taskList = document.querySelector(".taskList");
 let currentDay = document.querySelector(".currentDay");
 let currentDate = document.querySelector(".currentDate");
-let li,span;
+let li, span;
 
 // Create a new Date object
 var Date = new Date();
@@ -30,42 +30,54 @@ currentDay.appendChild(document.createTextNode(dayOfWeek));
 console.log("Current Day:", dayOfWeek);
 
 //function to add task
-function addTask(){
-  if(inputText.value==''){
+function addTask() {
+  if (inputText.value == "") {
     alert("Add some task ");
-  }
-  else{
+  } else {
     li = document.createElement("li");
     li.classList.add("task");
-    li.innerHTML = inputText.value;
-
+    //creating a div
+    div = document.createElement("div");
+    div.classList.add("task-text");
+    div.innerHTML=inputText.value;
+    li.appendChild(div);
+    //Creating a span 
     span = document.createElement("span");
+    span.classList.add("cross");
     span.innerHTML = "&#10006;"; // Unicode character for "X"
     li.appendChild(span);
     taskList.appendChild(li);
+    // console.log(li);
     inputText.value = "";
   }
 }
 //New task is added when clicked on plus icon
-button.addEventListener('click',addTask);
-inputText.addEventListener("keyup",function(e){
-    if(e.key=='Enter'){
-        addTask();
-    }
-})
+button.addEventListener("click", addTask);
+inputText.addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    addTask();
+  }
+});
 
-  // Event listener for the dynamically created li elements
-  taskList.addEventListener("click", function (event) {
-    const clickedElement = event.target;
+// Event listener for the dynamically created li elements
 
-    if (clickedElement.classList.contains("task")) {
-      // Toggle the "checked" class on the clicked li element
-      clickedElement.classList.toggle("checked");
-    }
+taskList.addEventListener("click", function (event) {
+  const clickedElement = event.target;
+  console.log(clickedElement);
+  // Toggle the "checked" class on the clicked li element
+ 
 
-    if (clickedElement.tagName === "SPAN") {
-      // Remove the parent li when the delete button is clicked
-      const liToRemove = clickedElement.parentNode;
-      liToRemove.remove();
-    }
-  });
+  if(clickedElement.classList.contains("task-text")){
+    clickedElement.parentNode.classList.toggle("checked");
+  }else{
+    clickedElement.classList.toggle("checked");
+  }
+
+  if (clickedElement.tagName === "SPAN") {
+    // Remove the parent li when the delete button is clicked
+    const liToRemove = clickedElement.parentNode;
+    liToRemove.remove();
+  }
+});
+
+
